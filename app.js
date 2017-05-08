3ed15d6673c48759b45e5fac661155fade6075ef
maraja <amit.maraj@gmail.com>
2017-05-08 16:10:05 -0400

    cookie expiry fix.

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // session handling
-app.use(session({ secret: config.session_secret, cookie: {1800 * 1000} })) // session lasts for half hour
+app.use(session({ secret: config.session_secret, cookie: {maxAge: 1800 * 1000} })) // session lasts for half hour
 // app.use(session({ secret: config.session_secret, cookie: {3600 * 1000} })) // session lasts for an hour
 
 app.use('/', index);
